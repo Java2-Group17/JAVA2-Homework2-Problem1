52505bf1b4a8141c0290fb2450b7c10e38efc927
jonathancuellar <jonathan.cuellar@mavs.uta.edu>
2014-10-11 11:23:12 -0500

    completed Date
    compiled Student and Date.
    fixes issue with compiling Student.java

---
+++ Date.java
@@ -0,0 +1,59 @@
+//Group 17
+
+//Jonathan Cuellar
+//Navinthira Raman
+//Luis Gardea
+//Ephraim Girmay
+//Gerardo Gonzalez-Inzunza
+
+public class Date
+{
+  private int month;
+  private int day;
+  private int year;
+
+  public Date(int m, int d, int y)
+  {
+    setMonth(m);
+    setDay(d);
+    setYear(y);
+  }
+
+  ///////get methods\\\\\\\
+  public int getMonth()
+  {
+    return month;
+  }
+
+  public int getDay()
+  {
+    return day;
+  }
+
+  public int getYear()
+  {
+    return year;
+  }
+
+  ///////set methods\\\\\\\
+  public void setMonth(int m)
+  {
+    month = m;
+  }
+
+  public void setDay(int d)
+  {
+    day = d;
+  }
+
+  public void setYear(int y)
+  {
+    year = y;
+  }
+
+  public String toString()
+  {
+      return("Month: " + month + " Day: " + day + " Year: " + year);
+  }
+
+} // end class
