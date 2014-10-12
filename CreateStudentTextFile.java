e709aba6713fd0090bcabb5097a8f82eec756fd2
jonathancuellar <jonathan.cuellar@mavs.uta.edu>
2014-10-11 21:21:52 -0500

    started CreateStudentTextFile

---
+++ CreateStudentTextFile.java
@@ -0,0 +1,25 @@
+//Group 17
+
+//Jonathan Cuellar
+//Navinthira Raman
+//Luis Gardea
+//Ephraim Girmay
+//Gerardo Gonzalez-Inzunza
+
+import java.io.FileNotFoundException
+import java.lang.SecurityException;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
+import java.util.NoSuchElementException;
+import javal.util.Scanner;
+
+public class CreateStudentTextFile
+{
+  private Formatter output;
+
+  public void openFile()
+  {
+
+  }
+
+} // end class
